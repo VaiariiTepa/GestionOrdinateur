41cb04cfdd94c49cbef6ecb63315cac6abe598af
VaiariiTepa <35317221+VaiariiTepa@users.noreply.github.com>
2019-04-11 02:27:50 -1000

    seeder computer fini

---
+++ database/seeds/ComputersTableSeeder.php
@@ -0,0 +1,74 @@
+<?php
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class ComputersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+
+        DB::table('computers')->insert([
+            'ref' => Str::random(10),
+        ]);
+    }
+}
