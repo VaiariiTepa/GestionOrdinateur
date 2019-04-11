fae8a3b2225df5d5d4fa01925915657a56545e2a
VaiariiTepa <35317221+VaiariiTepa@users.noreply.github.com>
2019-04-11 02:13:08 -1000

    Migration des tables fini

--- database/migrations/2019_04_11_112736_create_assignment_table.php
+++ database/migrations/2019_04_11_112736_create_assignment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAssignmentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('computersassignments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email', 250)->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('visitor_id');
+            $table->integer('computer_id');
+            $table->integer('openday_id');
+            $table->integer('open');
+            $table->integer('close');
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('computersassignments');
     }
 }
